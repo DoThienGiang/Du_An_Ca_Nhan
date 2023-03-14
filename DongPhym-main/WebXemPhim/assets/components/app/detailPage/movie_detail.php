f0347a0fb3169f508381f39e79e659f94f0113f2
Đỗ Thiên Giang <dothiengiang1994@gmail.com>
2023-03-14 20:12:16 +0700

    first commit

---
+++ DongPhym-main/WebXemPhim/assets/components/app/detailPage/movie_detail.php
@@ -0,0 +1,120 @@
+
+<div class = "detail-movie-background" style="background-image: url('https://3.bp.blogspot.com/-HWH2lhD6nNk/YTELhshgSoI/AAAAAAAAR7A/Z6ywaFi4KAAAIuH6LOIIYrTQtQ-YjIlogCNcBGAsYHQ/w1920-h445-p-k-no-nu/bg-min.jpg');" >
+    <div class="grid wide">
+        <div class="row detail-movie-content">
+            <div class="col l-2 m-4 c-6 detail-movie-img-wrapper">                
+                <img src="https://dongphymtv.com/public/files/flim/120x160/1639496297.jpg" alt="hoa tuyết điểm" class="detail-movie_img">                
+                <div class="interactive">
+                    <button class="interactive-btn  interactive-btn--liked">
+                        <i class="liked__icon fas fa-check"></i>
+                        <i class="like__icon fas fa-thumbs-up"></i>
+                        Like
+                        <span class="interactive-like-quantity">906</span>
+                    </button>
+                    <a href="" class="interactive-btn interactive-btn-share">Share</a>
+                </div>
+                <a href="" class="play-movie-btn">
+                    <i class="play__icon far fa-play-circle"></i>
+                    <span>Xem phim</span>
+                </a>
+            </div>
+            <div class="col l-10 m-8 c-12">
+                <h2 class="movies-title">Hoa tuyết điểm</h2>
+                <span class="movies-eng-title">Snowdrop (2021)</span>
+                <div class="episode-wrapper">
+                    <div class="episode__label">
+                        <i class="episode__icon fas fa-film"></i>
+                        <span class="episode__text">Server V.I.P</span>
+                    </div>
+                    <ul class="episode__list scrollbar">
+                        <li class="episode__item"><a href="" class="episode__link">1</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">2</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">3</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">4</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">5</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">6</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">7</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">8</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">9</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">10</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">11</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">12</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">13</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">14</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">15</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">16</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">17</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">18</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">19</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">20</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">21</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">22</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">23</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">24</a></li>
+                        <li class="episode__item"><a href="" class="episode__link">25</a></li>
+                    </ul>
+                </div>
+                <div class="row movie__info">
+                    <div class="col l-4 m-12 c-12">
+                        <div class="movie__info-content">
+                            <span class="movie__info-label">Thể loại: </span>
+                            <span class="movie__info-desc">Tình cảm</span>
+                        </div>
+
+                        <div class="movie__info-content">
+                            <span class="movie__info-label">Trạng thái: </span>
+                            <span class="movie__info-desc">Tập 25 vietsub</span>
+                        </div>
+
+                        <div class="movie__info-content">
+                            <span class="movie__info-label">Sắp chiếu: </span>
+                            <span class="movie__info-desc">tập 26 vietsub</span>
+                        </div>
+                    </div>
+                    <div class="col l-4 m-12 c-12">
+                        <div class="movie__info-content">
+                            <span class="movie__info-label">Đạo Diễn: </span>
+                            <span class="movie__info-desc">Jo Hyun-Tak</span>
+                        </div>
+
+                        <div class="movie__info-content">
+                            <span class="movie__info-label">Diễn Viên: </span>
+                            <span class="movie__info-desc">Kim Hye Yoon , Jisoo ,Jung Hae In, Jang Seung Jo, Yoon Se Ah Và Jung Yoo Jin...</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row showtime-rating">
+                    <div class="col l-6 m-12 c-12 showtime">
+                        <span>Lịch chiếu: VietSub 23h Thứ 7, Chủ nhật hàng tuần trên DongPhymtv.Com</span>
+                    </div>
+
+                    <div class="col l-6 m-12 c-12 rating">
+                        <div class="rating__wrapper">
+                            <div class="rating__star">
+                                <i class="star__icon star__icon--active-hover fas fa-star"></i>
+                                <i class="star__icon star__icon--active-hover fas fa-star"></i>
+                                <i class="star__icon star__icon--active-hover fas fa-star"></i>
+                                <i class="star__icon star__icon--active-hover fas fa-star"></i>
+                                <i class="star__icon star__icon--active-hover fas fa-star"></i>
+                                <i class="star__icon fas fa-star"></i>
+                                <i class="star__icon fas fa-star"></i>
+                                <i class="star__icon fas fa-star"></i>
+                                <i class="star__icon fas fa-star"></i>
+                                <i class="star__icon fas fa-star"></i>
+                            </div>  
+
+                            <span class="rating__info">(8.44 điểm / 1016 lượt)</span>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
