20808d0357aa2b21114184fb998e05cb06a90154
DoThienGiang <dothiengiang1994@gmail.com>
2024-07-24 16:23:36 +0700

    first commit

---
+++ Circle_progress_bar/assets/js/index.js
@@ -0,0 +1,10 @@
+let bikes = ['yamaha', 'bajaj', 'Honda', 'TVS'];
+// console.log(bikes.find());
+// console.log(bikes.toString());
+console.log(bikes.join());
+console.log(bikes.join(''));
+console.log(bikes.join('-'));
+
+console.log(bikes.toString());
+1;
+aaaaaaaaaaaaaaaaa;
---
+++ Layout-Onsen-UI/README.md
@@ -0,0 +1,3 @@
+## Code thanh Tabbar sử dụng onsen UI
+
+![alt](https://drive.google.com/file/d/1MA7_mLFKCPQAmBoHFpQeBdLOIEnPoygZ/view?usp=drive_link)
